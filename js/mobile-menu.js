ee98a7ccfbe83fa92b3ebd01a7e35b1abfa871fe
Verena Lambrecht <verena.lambrecht@gmail.com>
2026-01-14 20:50:26 +0100

    New design clos and linktree

--- js/mobile-menu.js
+++
@@ -1,7 +0,0 @@
-// document.querySelector(".burger").addEventListener("click", toggleMenu);
-// document.querySelector("header nav ul").addEventListener("click", toggleMenu);
-
-// function toggleMenu() {
-//     var element = document.querySelector("header nav ul");
-//     element.classList.toggle("open");
-// }
